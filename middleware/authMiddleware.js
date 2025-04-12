277013c3f71188fc94f8dc7d6b298dba4775f02d
Carlosgg8 <Carlosmartinez.20@icloud.com>
2025-04-11 23:42:20 -0700

    implemented the ensureAuthenticated function in side the authMiddleware.js file

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -26,5 +26,14 @@
  */
 export const ensureAuthenticated = (req, res, next) => {
     // TODO: Implement the function
+    // Check if the user is authenticated
+    if (req.isAuthenticated()) {
+      // If authenticated, proceed to the next middleware or route handler
+      return next();
+    } else  {
+      // If not authenticated, send a 401 Unauthorized response
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    
   };
   
